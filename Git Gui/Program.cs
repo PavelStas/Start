b2d9b826581505a653aa5c0638fb8aba3afd6cfc
Pavel Kor <ripavlik.koreh2016@yandex.ru>
2020-03-05 13:38:28 +0300

    Give a personalized hello.

--- Git Gui/Program.cs
+++ Git Gui/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Git_Gui
+namespace Git
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string name;
+            Console.WriteLine("Enter your name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+            Console.ReadLine();
         }
     }
 }
